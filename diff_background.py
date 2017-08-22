b357fc0e4f23f9feec3939378cbdb7c37ecdba5f
matsu hiroto <5bji1214@mail.u-tokai.ac.jp>
2017-08-22 13:58:08 +0900

    背景から差分表示

--- diff_background.py
+++ diff_background.py
@@ -1,42 +1,54 @@
-# http://lang.sist.chukyo-u.ac.jp/classes/OpenCV/py_tutorials/py_video/py_bg_subtraction/py_bg_subtraction.html
-# http://qiita.com/daxanya1/items/85f5e17ecc1203f756ad
-# https://www.blog.umentu.work/python-opencv3%E3%81%A7%E3%83%9E%E3%82%B9%E3%82%AF%E5%87%A6%E7%90%86%E3%81%97%E3%81%A6%E3%81%BF%E3%82%8B%EF%BC%88%E3%81%8A%E3%81%BE%E3%81%91%E3%81%82%E3%82%8A%EF%BC%89/
-
 import cv2
 import numpy as np
 
-cam = cv2.VideoCapture(0)
-height = int(cam.get(cv2.CAP_PROP_FRAME_HEIGHT))
-width = int(cam.get(cv2.CAP_PROP_FRAME_WIDTH))
+# 定数定義
+ESC_KEY = 27     # Escキー
+INTERVAL= 33     # インターバル
+FRAME_RATE = 30  # fps
 
-# 赤色で塗りつぶした画像を作成
-red = np.tile(np.uint8([255, 0, 255]), (height, width, 1))
+WINDOW_ORG = "org"
+WINDOW_BACK = "back"
+WINDOW_DIFF = "diff"
 
 
-# 背景差分取得
-bsub = cv2.createBackgroundSubtractorMOG2(detectShadows=False)
-count = 0
-try:
-    while(1):
-        count = (count + 1) % 10
-        ret, frame = cam.read()
-        if ret == False:
-            break
+# ウィンドウの準備
+cv2.namedWindow(WINDOW_ORG)
+cv2.namedWindow(WINDOW_BACK)
+cv2.namedWindow(WINDOW_DIFF)
 
-        mask = bsub.apply(frame, learningRate=0.01)
-        redfilter = cv2.bitwise_and(red, red, mask=mask)
+# 元ビデオファイル読み込み
+mov_org = cv2.VideoCapture('pose.avi')
 
-        # 赤色で点滅させる
-        if count < 5:
-            result = cv2.bitwise_or(frame, redfilter)
-        else:
-            result = frame
-    
-        cv2.imshow('result', result)
-        key = cv2.waitKey(30)
-        if key == ord('q'):
-            break
-finally:
-    cv2.destroyAllWindows()
-    cam.release()
+# 最初のフレーム読み込み
+has_next, i_frame = mov_org.read()
 
+# 背景フレーム
+back_frame = np.zeros_like(i_frame, np.float32)
+
+# 変換処理ループ
+while has_next == True:
+    # 入力画像を浮動小数点型に変換
+    f_frame = i_frame.astype(np.float32)
+
+    # 差分計算
+    diff_frame = cv2.absdiff(f_frame, back_frame)
+
+    # 背景の更新
+    cv2.accumulateWeighted(f_frame, back_frame, 0.025)
+
+    # フレーム表示
+    cv2.imshow(WINDOW_ORG, i_frame)
+    cv2.imshow(WINDOW_BACK, back_frame.astype(np.uint8))
+    cv2.imshow(WINDOW_DIFF, diff_frame.astype(np.uint8))
+
+    # Escキーで終了
+    key = cv2.waitKey(INTERVAL)
+    if key == ESC_KEY:
+        break
+
+    # 次のフレーム読み込み
+    has_next, i_frame = mov_org.read()
+
+# 終了処理
+cv2.destroyAllWindows()
+mov_org.release()
