7bb0cb13f318373ce46f9386f80087f5c3561e10
matsu hiroto <5bji1214@mail.u-tokai.ac.jp>
2017-08-22 16:21:37 +0900

    背景固定にし,グレースケール化

--- diff_background.py
+++ diff_background.py
@@ -12,34 +12,38 @@ WINDOW_DIFF = "diff"
 
 
 # ウィンドウの準備
-cv2.namedWindow(WINDOW_ORG)
-cv2.namedWindow(WINDOW_BACK)
 cv2.namedWindow(WINDOW_DIFF)
 
-# 元ビデオファイル読み込み
-mov_org = cv2.VideoCapture('pose.avi')
+# カーネル設定
+kernel = np.ones((2,2),np.uint8)
 
-# 最初のフレーム読み込み
-has_next, i_frame = mov_org.read()
 
-# 背景フレーム
-back_frame = np.zeros_like(i_frame, np.float32)
+
+# 背景ビデオファイル読み込み
+cam1 = cv2.VideoCapture('bg.avi')
+# グレースケール
+back_frame = cv2.cvtColor(cam1.read()[1], cv2.COLOR_RGB2GRAY)
+
+
+
+# 比較ビデオファイル読み込み
+cam = cv2.VideoCapture('pose.avi')
+# グレースケール化
+org_frame = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
+
+
 
 # 変換処理ループ
-while has_next == True:
-    # 入力画像を浮動小数点型に変換
-    f_frame = i_frame.astype(np.float32)
+while True:
 
     # 差分計算
-    diff_frame = cv2.absdiff(f_frame, back_frame)
+    diff_frame = cv2.absdiff(org_frame, back_frame)
 
-    # 背景の更新
-    cv2.accumulateWeighted(f_frame, back_frame, 0.025)
+    diff_frame = cv2.morphologyEx(diff_frame, cv2.MORPH_OPEN, kernel)
 
-    # フレーム表示
-    cv2.imshow(WINDOW_ORG, i_frame)
-    cv2.imshow(WINDOW_BACK, back_frame.astype(np.uint8))
-    cv2.imshow(WINDOW_DIFF, diff_frame.astype(np.uint8))
+    ### フレーム表示 ###
+    ## 差分動画表示
+    cv2.imshow(WINDOW_DIFF, diff_frame)
 
     # Escキーで終了
     key = cv2.waitKey(INTERVAL)
@@ -47,8 +51,7 @@ while has_next == True:
         break
 
     # 次のフレーム読み込み
-    has_next, i_frame = mov_org.read()
+    org_frame = cv2.cvtColor(cam.read()[1], cv2.COLOR_RGB2GRAY)
 
 # 終了処理
 cv2.destroyAllWindows()
-mov_org.release()
