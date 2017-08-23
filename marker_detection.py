964480e217d6011917df033a7e58ea5372be9b8d
matsu hiroto <5bji1214@mail.u-tokai.ac.jp>
2017-08-23 15:31:30 +0900

    テンプレートマッチング

--- marker_detection.py
+++ marker_detection.py
@@ -3,24 +3,28 @@
 
 import cv2
 import numpy as np
+from matplotlib import pyplot as pyplot
+
 
 def detect_marker():
-    cam = cv2.VideoCapture(0)
+
+    img_rgb = cv2.imread('MarioVSKuppa.jpg')
+    img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+    template = cv2.imread('fire.jpg', 0)
+    w, h = template.shape[::-1]
+
+    res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
+    threshold = 0.8
+    loc = np.where( res >= threshold)
+    for pt in zip(*loc[::-1]):
+        cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 4)
+    cv2.imshow("Image", img_rgb) 
     while True:
-        ret, img = cam.read()
-
-
-
-        # 表示
-        cv2.imshow("Image", img)
-
-        
-        key = cv2.waitKey(10)
-        if key is ord('q'):
+        # qを押したら終了。
+        k = cv2.waitKey(1)
+        if k == ord('q'):
             break
-
-    cv2.destroyAllWindows()
-    cam.release()
 
 
 
