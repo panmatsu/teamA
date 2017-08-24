3d378eca7a25dbd38c86796dd9b703f9cc4db9eb
matsu hiroto <5bji1214@mail.u-tokai.ac.jp>
2017-08-24 10:51:11 +0900

    コメント、コンソール確認追加

--- marker_detection.py
+++ marker_detection.py
@@ -1,24 +1,39 @@
 # 参考サイト
 # http://qiita.com/sbtseiji/items/6438ec2bf970d63817b8
+# http://labs.eecs.tottori-u.ac.jp/sd/Member/oyamada/OpenCV/html/py_tutorials/py_imgproc/py_template_matching/py_template_matching.html
+# https://matplotlib.org/users/installing.html # matplotlibのインストール
+#
 
 import cv2
 import numpy as np
 from matplotlib import pyplot as pyplot
 
-
+####  マーカー検地  ####
 def detect_marker():
-
-    img_rgb = cv2.imread('MarioVSKuppa.jpg')
+    ## 元画像読み込み
+    img_rgb = cv2.imread('7.png')
+    # グレースケール化し、保存
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
-    template = cv2.imread('fire.jpg', 0)
+
+    ## テンプレート画像読み込み
+    template = cv2.imread('hand.png', 0)
+    # 画像サイズ取得
     w, h = template.shape[::-1]
 
+    # テンプレートマッチング
     res = cv2.matchTemplate(img_gray, template, cv2.TM_CCOEFF_NORMED)
-    threshold = 0.8
+    # マッチ度
+    threshold = 0.7
+    # マッチ度より高ければ場所保存
     loc = np.where( res >= threshold)
+
+    # マッチした数だけ
     for pt in zip(*loc[::-1]):
+        print("左上:"+str(pt[0]))
+        # 左上座標と右下座標から四角形表示
         cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 4)
+    # 画像表示
     cv2.imshow("Image", img_rgb) 
     while True:
         # qを押したら終了。
