f79e319aeb0b7fa7383f1006d1dfc266bdfba31d
hirotohirama <s1531127gb@s.chibakoudai.jp>
2017-08-24 10:08:27 +0900

    背景との差分処理

--- backgroundSub.py
+++ backgroundSub.py
@@ -16,14 +16,21 @@ frame = cv2.imread(sys.argv[2],0)
 fgmask = cv2.absdiff(frame,cap)
 
 #２値化の閾値
-t = 25
+t = 50
 ret, binal = cv2.threshold(fgmask, t, 255, cv2.THRESH_BINARY)
+
+# 平滑下
+binal = cv2.medianBlur(binal, 5)
+
 #ノイズ消しのパラメータ
 kernel = np.ones((5,5),np.uint8)
 erosion = cv2.erode(binal,kernel,iterations = 1)
-#穴埋めのパラメータ
+#ノイズ処理
 opening = cv2.morphologyEx(binal, cv2.MORPH_OPEN, kernel)
+
+#穴埋めパラメータ
 kernel = np.ones((5,5),np.uint8)
+#穴埋め処理
 result = cv2.morphologyEx(opening, cv2.MORPH_CLOSE, kernel)
 
 #(0,0,0)でない部分の面積
