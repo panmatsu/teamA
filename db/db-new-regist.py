3e874ba40961d815204565ec0e4088f887caf9ba
ShrMatsuoka <s1511560@s.tsukuba.ac.jp>
2017-08-29 17:26:04 +0900

    Add change

--- db/db-new-regist.py
+++ db/db-new-regist.py
@@ -34,45 +34,65 @@ while(True):
         file_name = "background.png"
         cv2.imwrite(file_name, gray)
         print("please push key｢v｣, and pose for a key!")
-        #key = cv2.waitKey(1)
-        if key == ord('v'):
-            videofile = nid + "_" + nname + ".avi"
-            frame_width = frame.shape[1]
-            frame_height = frame.shape[0]
-            four_cc = cv2.VideoWriter_fourcc(*'XVID')
-            fps = 30.0
-            writer = cv2.VideoWriter(videofile, four_cc, fps, (frame_width, frame_height))
+    elif key == ord('v'):
+        output_filename = str(nid) + "_" + nname + ".avi"
+        frame_width = frame.shape[1]
+        frame_height = frame.shape[0]
+        four_cc = cv2.VideoWriter_fourcc(*'XVID')
+        fps = 30.0
+        writer = cv2.VideoWriter(output_filename, four_cc, fps, (frame_width, frame_height))
+        # record first frame.
+        writer.write(frame)
 
+        while True:
+            _, img = cap.read()
+            if img is None:
+                break
+            cv2.imshow('capture', img)
             writer.write(img)
-
-            while True:
-                _, img = capturer.read()
-                if img is None:
-                    break
-                cv2.imshow('capture', img)
-                writer.write(img)
-                k = cv2.waitKey(10)
-                if k is ord('q'):
-                    break
+            k = cv2.waitKey(10)
+            if k is ord('q'):
+                break
 
         cap.release()
-        #writer.release()
+        writer.release()
+
+recap = cv2.videoCapture(output_filename)
+ret, poseframe = recap.read()
+cnt = 0
+
+while(True):
+    if poseframe is None:
+        print("FINISH!!")
+        break
+
+    gray = cv2.cvtColor(poseframe, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('poseframe',gray)
+    pkey = cv2.waitkey(1)
+    if pkey == ord('q'):
+        break
+    elif pkey == ord('n'):
+        ret, poseframe = recap.read()
+    elif key == ord('s'):
+        file_name = "gray_pose"
+        cv2.imwright(file_name,gray)
+        cnt += 1
+recap.release()
 
 
-    ##### 動画とって好きなタイミングのを保存
-    #### マーカー範囲指定　rt/rb/lt/lb
+    #### マーカー範囲指定
     #### 読み込むなりなんなりして取得、仮設定中
-    left_ltx = 0
-    left_lty = 0
-    left_rbx = 0
-    left_rby = 0
-    right_ltx = 0
-    right_lty = 0
-    right_rbx = 0
-    right_rby = 0
+left_ltx = 0
+left_lty = 0
+left_rbx = 0
+left_rby = 0
+right_ltx = 0
+right_lty = 0
+right_rbx = 0
+right_rby = 0
 #### ポーズの画像
 #pose = sys.argv[7]
-    pose = "s1.png"
+pose = "s1.png"
 
 #        file_name = str(nid) + ".png"
 #    print(file_name)
