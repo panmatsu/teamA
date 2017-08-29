29c4c6f0f5e9985990d2ea27001a8be2150b9f6f
ShrMatsuoka <s1511560@s.tsukuba.ac.jp>
2017-08-29 16:29:38 +0900

    Some change

--- db/db-new-regist.py
+++ db/db-new-regist.py
@@ -1,9 +1,12 @@
 # -*- codeing: utf-8 -*-
 
+import numpy as np
 import sqlite3
 import sys
 import cv2
 import shutil
+import os
+import time
 
 # コマンドラインより登録データ取得(idと名前)
 print("please write your ID >")
@@ -30,33 +33,61 @@ while(True):
     elif key == ord('s'):
         file_name = "background.png"
         cv2.imwrite(file_name, gray)
-        move = "img/" + file_name
-        shutil.move(file_name, move)
+        print("please push key｢v｣, and pose for a key!")
+        #key = cv2.waitKey(1)
+        if key == ord('v'):
+            videofile = nid + "_" + nname + ".avi"
+            frame_width = frame.shape[1]
+            frame_height = frame.shape[0]
+            four_cc = cv2.VideoWriter_fourcc(*'XVID')
+            fps = 30.0
+            writer = cv2.VideoWriter(videofile, four_cc, fps, (frame_width, frame_height))
 
-        print("please pose for a key!")
+            writer.write(img)
 
-##### 3秒人物認証→撮影とか？
-#### マーカー範囲指定　rt/rb/lt/lb
-#### 読み込むなりなんなりして取得、仮設定中
-rt = 0
-rb = 0
-lt = 0
-lb = 0
+            while True:
+                _, img = capturer.read()
+                if img is None:
+                    break
+                cv2.imshow('capture', img)
+                writer.write(img)
+                k = cv2.waitKey(10)
+                if k is ord('q'):
+                    break
+
+        cap.release()
+        #writer.release()
+
+
+    ##### 動画とって好きなタイミングのを保存
+    #### マーカー範囲指定　rt/rb/lt/lb
+    #### 読み込むなりなんなりして取得、仮設定中
+    left_ltx = 0
+    left_lty = 0
+    left_rbx = 0
+    left_rby = 0
+    right_ltx = 0
+    right_lty = 0
+    right_rbx = 0
+    right_rby = 0
 #### ポーズの画像
 #pose = sys.argv[7]
-pose = "s1.png"
+    pose = "s1.png"
 
 #        file_name = str(nid) + ".png"
-#        print(file_name)
-#        cv2.imwrite(file_name, gray)
-#        move = "img/" + file_name
-#        shutil.move(file_name, move)
+#    print(file_name)
+#    cv2.imwrite(file_name, gray)
+#    move = "img/" + file_name
+#    shutil.move(file_name, move)
 
 # 同フォルダ内のdbkey.dbのDBを展開
 con = sqlite3.connect("dbkey.db", isolation_level=None)
-
-sql = "insert into keyset values (" + str(nid) + ", '" + nname + "', " + str(rt) + ", " + str(rb) + ", " + str(lt) + ", " + str(lb) + ", '" + pose + "')"
+sqlleft = str(left_ltx) + ", " + str(left_lty) + ", " + str(left_rbx) + ", " + str(left_rby)
+sqlright = str(right_ltx) + ", " + str(right_lty) + ", " + str(right_rbx) + ", " + str(right_rby)
+sql = "insert into keyset values (" + str(nid) + ", \'" + nname + "\', " + sqlleft + ", " + sqlright + ", \'" + pose + "\')"
 print(sql)
 ## 実行
 #con.execute(sql)
 con.close()
+
+
