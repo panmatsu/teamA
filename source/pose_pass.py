b1d80bf8b36cfba3288783cb9e3198ff9ae428d1
hirotohirama <s1531127gb@s.chibakoudai.jp>
2017-08-31 14:04:10 +0900

    パラメータ修正

--- source/pose_pass.py
+++ source/pose_pass.py
@@ -112,7 +112,7 @@ while(1):
 
     #人物認識実行
     if marker_flag == False:
-        human,r = hog.detectMultiScale(proFrame,**hogParams)
+        human,r = hog.detectMultiScale(proFrame,hitThreshold=-0.5, winStride=(8,8), padding=(0,0), scale=1.05, finalThreshold=5)
         face = cascade.detectMultiScale(proFrame, scaleFactor=1.2, minNeighbors=2, minSize=(10, 10))
         
         if len(human) != 0:
