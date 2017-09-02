412dfa9e107801d2750504fc8c4b94c8eea59b9d
ヒラマヒロト <hiramahiroto@hiramahiroto-no-iMac.local>
2017-09-03 05:53:58 +0900

    プログラム書き直し

---
+++ ブラッシュアップ/TimeCount.py
@@ -0,0 +1,82 @@
+import time
+import cv2
+import numpy as np
+
+import PersonAuthentication
+import MarkerJudgement
+import MarkerDetection
+
+#####################################
+#特定動作を任意の時間続けているか判定
+####################################
+
+class TimeCount:
+
+    #キープ秒数
+    t = 3.0
+
+    #秒数判定の最低フレーム
+    f = 10
+
+    start_time = 0.0
+
+    frame_count = 0
+
+
+    def __init__(self,ti,fr):
+        
+        self.t = ti
+        self.f = fr
+
+    def personKeep(self,pd,img,log):
+        
+        ret,frame = pd.personDetect(img,log)
+        cv2.imshow('detect:person',frame)
+
+        if ret == True and self.start_time == 0.0:
+            self.start_time = time.time()
+            log.write("start time measurement(person)\n")
+        if ret == True and self.start_time != 0.0:
+            self.frame_count += 1
+
+        if self.start_time != 0 and time.time()-self.start_time > self.t and self.frame_count > self.f:
+            self.start_time = 0.0
+            self.frame_count = 0
+            log.write("person key open\n")
+            return True
+
+        elif self.start_time != 0 and time.time()-self.start_time > self.t and self.frame_count > self.f:
+            self.start_time = 0.0
+            self.frame_count = 0
+            log.write("person key close\n")
+            return False
+
+        else:
+            return False
+
+    def markerKeep(self,mj,md,img,log):
+        
+        ret,frame = mj.judge_marker(md,img,log)
+        cv2.imshow('detect:marker',frame)
+        
+        if ret == True and self.start_time == 0.0:
+            self.start_time = time.time() 
+            log.write("start time measurement(marker)\n") 
+        if ret == True and self.start_time != 0.0:
+            self.frame_count += 1
+  
+        if self.start_time != 0 and time.time()-self.start_time > self.t and self.frame_count > self.f: 
+            self.start_time = 0.0
+            self.frame_count = 0
+            log.write("marker key open\n")
+            return True 
+        
+        elif self.start_time != 0 and time.time()-self.start_time > self.t and self.frame_count > self.f:
+            self.start_time = 0.0
+            self.frame_count = 0
+            log.write("marker key close\n")
+            return False
+        
+        else:
+            return False
+
