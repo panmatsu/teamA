133a81019ace17714373eda58d62420c9a4b78d8
ヒラマヒロト <hiramahiroto@hiramahiroto-no-iMac.local>
2017-09-03 19:04:45 +0900

    クラス化

---
+++ ブラッシュアップ/pose_pass.py
@@ -0,0 +1,116 @@
+###################
+#main
+##################
+import cv2 
+import numpy as np 
+import os
+import time
+import sys
+
+import MarkerDetection 
+import MarkerJudgement 
+import Key 
+import PersonAuthentication
+import TimeCount
+import PoseDetection
+
+if len(sys.argv) == 1:
+    cam = cv2.VideoCapture(0)
+    ret, back = cam.read()
+    if ret == False:
+        print('cannot read cam')
+        exit(0)
+    else:
+        #背景画像取得
+        back = cv2.cvtColor(back, cv2.COLOR_RGB2GRAY)
+if len(sys.argv) == 2:
+    cam = cv2.VideoCapture(sys.argv[1])
+    #背景画像取得 
+    back = cv2.imread('back.png',0)
+    
+#ログテキスト 
+log = open("log.txt","w")
+
+#インスタンス化
+md = MarkerDetection.MarkerDetection()
+mj = MarkerJudgement.MarkerJudgement()
+pd = PersonAuthentication.PersonAuthentication()
+
+#TimeCount(1,2)
+#1:何秒間そのポーズを保つか
+#2:↑の間に何フレームそのポーズをとっているか
+tc = TimeCount.TimeCount(3.0,10)
+
+#PoseDetection(1,2,3,4)
+#1:２値化パラメータ
+#2:#平滑化パラメータ
+#3:ポーズ判定ピクセル数 
+#4:フレーム取得回数
+sb = PoseDetection.PoseDetection(50,5,15000,10)
+
+#######--dbから鍵参照--#######
+key = Key.Key()
+while(1):
+    ret,db_pose,name = key.getKey(mj)
+    if ret == True:
+        break
+
+#鍵画像を取得
+pose_key = cv2.imread(db_pose,0)
+
+print("Are you "+name+"?")
+log.write("get db's data\n")
+
+#フェーズ移行フラッグ
+person_flag = False
+marker_flag = False
+
+#ポーズ認証を３回ミスしたら終了
+count = 0
+
+while(1):
+    ret,frame = cam.read()
+    if ret == False:
+        break
+    cp_frame = frame.copy()
+
+    if person_flag == False:
+        r,cp_frame = pd.personDetect(cp_frame,log)
+        person_flag = tc.keep(r,cp_frame)
+        cv2.imshow('detect:person',cp_frame)
+        if person_flag == True:
+            log.write("detect person\n")
+        else:
+            log.write("cannot detect person\n")
+
+    if person_flag == True:
+        r,cp_frame = mj.judge_marker(md,cp_frame,log)
+        marker_flag = tc.keep(r,cp_frame)
+        cv2.imshow('detect:marker',cp_frame)
+        if marker_flag == True:
+            log.write("marker key open\n")
+        else:
+            log.write("marker key close\n")
+    
+    if marker_flag == True:
+        j = sb.judge_pose(frame,back,pose_key,log)
+        if j == 1:
+            print('open you are '+name)
+            break
+        elif j == 2:
+            marker_flag = False
+            count += 1
+            print('close')
+        elif count >= 3:
+            print('close you are not '+name)
+            break
+            
+    cv2.imshow('result',frame)
+    k = cv2.waitKey(30)
+    if k == ord('q'):
+        break
+log.close()
+cv2.destroyAllWindows()
+cam.release()
+
+    
